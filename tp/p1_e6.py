9f804287e63a6b4ec088cefdf3cbef7fab576e1d
Facundo Loser <loserfacundodario@gmail.com>
2025-10-25 15:19:02 -0300

    corrigiendo un par de errores

--- tp/p1_e6.py
+++ tp/p1_e6.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # un grafo completo y no dirigido.
 
 # hiperparametros
-POPULATION_SIZE = 24  # tamaño de la población. El limite es la cant de permutaciones de las ciudades sin contar de la que se parte
+POPULATION_SIZE = 30  # tamaño de la población. El limite es la cant de permutaciones de las ciudades sin contar de la que se parte
 GENERATIONS = 40      # cantidad de generaciones a evolucionar
 CROSOVER_PROB = 0.8   # probabilidad de crossover
 MUTATION_PROB = 0.04  # probabildiad de mutación
@@ -48,19 +48,20 @@ creator.create("Individual", list, fitness=creator.FitnessMin)
 toolbox = base.Toolbox()
 
 # para crear la población inicial lo que hago es obtener una lista con todas
-# las ciudades menos la ciudad de arranque. Luego genero todas las permutaciones
-# de esa lista y al final me quedo solo con tantas permutaciones como tamaño
-# de población tenga concatenando al principio y al final la ciudad de inicio
+# las ciudades menos la de arranque y luego voy generando una permutación
+# aleatoria hasta completar la población. Al final antes de agregar un individuo
+# le concateno la ciudad de donde se parte al principio y la final.
 def create_population():
     cities = list(range(CANT_CITIES))
     cities.remove(START_CITY)
-    permutaciones = list(itertools.permutations(cities))
-    permutaciones = [list(p) for p in permutaciones]
-
     poblacion = []
-    for i in range(POPULATION_SIZE):
-        ruta = [START_CITY] + permutaciones[i] + [START_CITY]
+
+    for _ in range(POPULATION_SIZE):
+        # genera una permutación aleatoria de las ciudades
+        ruta_intermedia = random.sample(cities, len(cities))
+        ruta = [START_CITY] + ruta_intermedia + [START_CITY]
         poblacion.append(creator.Individual(ruta))
+
     return poblacion
 
 # la población es una lista de individuos
@@ -94,50 +95,48 @@ def eval(individual):
 
 toolbox.register("evaluate", eval)
 
-# función de crossover usando 1 point. primero se excluye la ciudad de inicio
-# para evitar crear individuos inválidos. Entonces se parten los 2 padres
-# (padre1 y padre2) en un punto. Luego para generar el hijo1 se toma el
-# primer pedazo del del padre1 y se rellena el resto con el padre2 de forma
-# que no queden repetidos para evitar individuos inválidos. Luego para
-# generar el hijo2 se toma la primer parte de padre2 y se rellena  el
-# con el padre1 de igual forma sin dejar ciudades repetidas.
+# función de crossover usando 1 point crossover modificado. primero se
+# excluye la ciudad de inicio para evitar crear individuos inválidos.
+# Entonces se parten los 2 padres (padre1 y padre2) en un punto. Luego
+# para generar el hijo1 se toma el primer pedazo del del padre1 y se rellena
+# el resto con el padre2 de forma que no queden repetidos para evitar
+# individuos inválidos. Luego para generar el hijo2 se toma la primer parte
+# de padre2 y se rellena con el resto del padre1 de igual forma sin dejar
+# ciudades repetidas.
 def crossover(p1, p2):
-    if random.random() < CROSOVER_PROB:
-        # excluyo la ciudad de inicio (y de fin)
-        p1_inner = p1[1:-1]
-        p2_inner = p2[1:-1]
+    # excluyo la ciudad de inicio (y de fin)
+    p1_inner = p1[1:-1]
+    p2_inner = p2[1:-1]
 
-        # genero el punto para partir:
-        point = random.randint(1, len(p1_inner) - 1)
+    # genero el punto para partir:
+    point = random.randint(1, len(p1_inner) - 1)
 
-        # genero la primer mitad de cada hijo:
-        offspring1 = [START_CITY] + p1_inner[:point]
-        offspring2 = [START_CITY] + p2_inner[:point]
+    # genero la primer mitad de cada hijo:
+    offspring1 = [START_CITY] + p1_inner[:point]
+    offspring2 = [START_CITY] + p2_inner[:point]
 
-        # ahora relleno el resto de cada hijo con el otro padre respectivamente:
-        for city in p2_inner:
-            if city not in offspring1: # chequeo para evitar ciudades repetidas
-                offspring1.append(city)
-        for city in p1_inner:
-            if city not in offspring2:
-                offspring2.append(city)
+    # ahora relleno el resto de cada hijo con el otro padre respectivamente:
+    for city in p2_inner:
+        if city not in offspring1: # chequeo para evitar ciudades repetidas
+            offspring1.append(city)
+    for city in p1_inner:
+        if city not in offspring2:
+            offspring2.append(city)
 
-        offspring1.append(START_CITY)
-        offspring2.append(START_CITY)
+    offspring1.append(START_CITY)
+    offspring2.append(START_CITY)
 
-        return creator.Individual(offspring1), creator.Individual(offspring2)
-    return creator.Individual(p1[:]), creator.Individual(p2[:]) # sino se clonan los padres
+    return creator.Individual(offspring1), creator.Individual(offspring2)
 
 # registro la función crossover
-toolbox.register("mate", crossover) # 2 point crosover
+toolbox.register("mate", crossover)
 
 # operador de mutación: intercambia 2 ciudades de lugar menos
 # la ciudad de inicio (y de fin que es la misma) para evitar individuos invalidos
 def mutate(individual):
-    if random.random() < MUTATION_PROB:
-        index1 = random.randint(1, len(individual)-2)
-        index2 = random.randint(1, len(individual)-2)
-        individual[index1], individual[index2] = individual[index2], individual[index1]
+    index1 = random.randint(1, len(individual)-2)
+    index2 = random.randint(1, len(individual)-2)
+    individual[index1], individual[index2] = individual[index2], individual[index1]
     return individual,
 
 # registro la función mutación
@@ -200,7 +199,7 @@ def main():
     stats.register("max", lambda fits: tuple(max(f) for f in zip(*fits)))
 
     # evolucionar
-    algorithms.eaMuPlusLambda(pop, toolbox, mu=50, lambda_=100, cxpb=0.7, mutpb=0.3, ngen=GENERATIONS, stats=stats, halloffame=hof, verbose=True)
+    algorithms.eaMuPlusLambda(pop, toolbox, mu=30, lambda_=30, cxpb=CROSOVER_PROB, mutpb=MUTATION_PROB, ngen=GENERATIONS, stats=stats, halloffame=hof, verbose=True)
     print("\n--- Frente de Pareto ---")
     for ind in hof:
         print(ind, ind.fitness.values)
